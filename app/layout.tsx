ac88713c446763afe419b5500b0fbede920a1be2
tlevesconte <tlevesconte@proton.me>
2024-08-29 23:34:42 +0200

    Add shadcn/ui and geist vercel fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import "./globals.css";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 
-const sitka = localFont({
-  src: "./sitka.ttf",
-  display: "swap",
-});
+import type { Metadata } from "next";
+import { cn } from "@/lib/utils"
+
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+// import localFont from "next/font/local";
+
+// const sitka = localFont({
+//   src: "./sitka.ttf",
+//   display: "swap",
+// });
 
 export const metadata: Metadata = {
   title: "Tomas Le Vesconte",
@@ -20,7 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${sitka.className} mx-auto my-[30vmin] max-w-3xl bg-[#1F1F1F] px-[6%]`}
+        className={cn(
+          "mx-auto my-[30vmin] max-w-3xl px-[6%] bg-background font-sans antialiased",
+          GeistSans.variable
+        )}
       >
         {children}
       </body>
