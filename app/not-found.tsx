ee562bf0988734bbd60feab64abe23167e1321c6
tlevesconte <tlevesconte@proton.me>
2024-09-02 16:13:12 +0200

    Add custom 404

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-row justify-center text-[15px]">
+      <h1 className="font-medium">404</h1>
+      <span className="ml-1 mr-1"> — </span>
+      <p>Page not found.</p>
+    </div>
+  );
+}
