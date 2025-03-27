d27a6a5f9c9b39f58e308d0ea53b635a2c440d78
tlevesconte <tlevesconte@proton.me>
2025-03-27 21:49:19 +0100

    Add small changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,9 @@
+import SpecialText from "../components/special-text";
+
 export default function NotFound() {
   return (
-    <div className="flex flex-row justify-center text-[15px]">
-      <h1 className="font-medium">404</h1>
+    <div className="flex flex-row justify-center">
+      <SpecialText text="404" />
       <span className="ml-1 mr-1"> — </span>
       <p>Page not found.</p>
     </div>
