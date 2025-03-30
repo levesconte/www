155545e3cd9c67316d69d09bbfeeba7d8a575acc
tlevesconte <tlevesconte@proton.me>
2025-03-30 19:52:52 +0200

    Change regular text (light) opacity value & switch icons

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,8 +11,8 @@ export default function ModeToggle() {
       className="ml-2 inline-flex h-9 w-9 items-center justify-center"
       onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
     >
-      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-primary-foreground transition-transform dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary-foreground transition-transform dark:rotate-0 dark:scale-100" />
+      <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-primary-foreground transition-transform dark:-rotate-90 dark:scale-0" />
+      <SunIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary-foreground transition-transform dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle light/dark mode</span>
     </button>
   );
