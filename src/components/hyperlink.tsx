72a0cce13ff3d821d9809ea84984b4f893b6c3f9
levesconte <tlevesconte@proton.me>
2025-04-24 15:12:44 +0200

    Remove space

--- src/components/hyperlink.tsx
+++ src/components/hyperlink.tsx
@@ -11,7 +11,7 @@ export default function Hyperlink({
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      aria-label={` ${text} (opens in a new tab)`}
+      aria-label={`${text} (opens in a new tab)`}
     >
       {text}
     </a>
