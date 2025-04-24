7170bc8860b2846ad3e910ae4b00f3a6265be059
levesconte <tlevesconte@proton.me>
2025-04-24 11:39:09 +0200

    Improve theme toggle implementation

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,43 +1,46 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export default function ThemeToggle() {
-  const DEFAULT_THEME = "dark";
-  
+  const FALLBACK_THEME = "dark";
+
   const loaded = useRef<boolean>(false);
-  const [theme, setTheme] = useState<string>(DEFAULT_THEME);
 
   useEffect(() => {
-    if (loaded.current) {
-      // removes
-      document.documentElement.classList.remove("dark", "light");
-      localStorage.removeItem("theme");
-      // adds
+    if (!loaded.current) {
+      const theme: string = localStorage.getItem("theme") ?? FALLBACK_THEME;
       document.documentElement.classList.add(theme);
       localStorage.setItem("theme", theme);
     }
-
-    if (!loaded.current && "theme" in localStorage) {
-      // fetch the localStorage theme value (if it exists),
-      // otherwise use a default value.
-      const value: string = localStorage.getItem("theme") ?? DEFAULT_THEME;
-      document.documentElement.classList.add(value);
-      setTheme(value); // sync state with local
-    }
     loaded.current = true;
-  }, [theme]);
+  }, []);
+
+  function toggleTheme() {
+    if (loaded.current) {
+      let theme: string = localStorage.getItem("theme") ?? FALLBACK_THEME;
+      theme = theme === "dark" ? "light" : "dark";
+
+      document.documentElement.classList.remove("dark", "light");
+      localStorage.removeItem("theme");
+
+      document.documentElement.classList.add(theme);
+      localStorage.setItem("theme", theme);
+    }
+  }
 
   return (
     <button
       className="ml-2 inline-flex h-9 w-9 items-center justify-center hover:cursor-pointer"
-      onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+      onClick={toggleTheme}
     >
       {/* Dark Icon */}
       <MoonIcon className="text-light-special-foreground h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-transform dark:scale-0 dark:-rotate-90" />
+
       {/* Light Icon */}
       <SunIcon className="dark:text-dark-special-foreground absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-transform dark:scale-100 dark:rotate-0" />
+
       {/* For accessibility */}
       <span className="sr-only">Toggle light/dark mode</span>
     </button>
